515dd884c6e2ff383e84527635ca07180f48ee1b
Reshmi <khatoonreshmi875@gmail.com>
2025-10-17 13:53:51 +0530

    github data added

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 const port = 3000;
-
+const github = {
+  login: "hiteshchoudhary",
+  id: 11678899,
+  node_id: "VGFGHK8655FHnnj6bJ",
+  url: "https://www.google.com",
+};
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -15,6 +20,9 @@ app.get("/login", (req, res) => {
 app.get("/youtube", (req, res) => {
   res.send("<h2>chai aur Code</h2>");
 });
+app.get("/github", (req, res) => {
+    res.json(github)
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`);
